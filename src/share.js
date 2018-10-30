eb5b155f4e92804c9d96b11fee88606bdcefeee7
László Horváth <bedafoltyn@gmail.com>
2018-10-30 22:36:24 +0100

    share script changed

--- src/share.js
+++ src/share.js
@@ -1,20 +1,30 @@
 const facebookShareButtons = Array.from(document.querySelectorAll('.social-button.facebook'))
 const tumblrShareButtons = Array.from(document.querySelectorAll('.social-button.tumblr'))
 const twitterShareButtons = Array.from(document.querySelectorAll('.social-button.twitter'))
+const url = document.URL
 
-function shareOnFacebook () {
-  FB.ui({
+function shareOnFacebook (e) {
+  /* FB.ui({
     method: 'share',
     href: window.location.href,
-  }, function(response){});
+  }, function(response){}); */
+  e.preventDefault();
+  var facebookWindow = window.open('https://www.facebook.com/sharer/sharer.php?u=' + document.URL, 'facebook-popup', 'height=350,width=600');
+  if(facebookWindow.focus) { facebookWindow.focus(); }
+    return false;
 }
 
 function shareOnTumblr () {
   console.log('shared on tumblr')
 }
 
-function shareOnTwitter () {
+function shareOnTwitter (e) {
   console.log('shared on twitter')
+  e.preventDefault();
+  var twitterWindow = window.open('https://twitter.com/share?url=' + document.URL, 'twitter-popup', 'height=350,width=600');
+  if(twitterWindow.focus) { twitterWindow.focus(); }
+    return false;
+
 }
 
 facebookShareButtons.forEach(button => button.addEventListener('click', shareOnFacebook))
