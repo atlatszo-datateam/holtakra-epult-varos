fe1fa416baeba5cf5bb480f7c1851763761faccc
László Horváth <bedafoltyn@gmail.com>
2018-10-31 10:20:11 +0100

    tumblr share modified

--- src/share.js
+++ src/share.js
@@ -13,7 +13,7 @@ function shareOnFacebook () {
 }
 
 function shareOnTumblr () {
-  const popup = window.open(`http://tumblr.com/widgets/share/tool?canonicalUrl=${URL}`, 'tumblr-popup', windowSize)
+  const popup = window.open(`https://tumblr.com/share/tool?shareSource=legacy&canonicalUrl=${URL}&posttype=link&title=Holtakra+épült+város&content=${URL}`, 'tumblr-popup', windowSize)
   if (popup.focus) popup.focus()
 
   return false
